d0b917d5462cb45130db22df4c061dc93999ba48
shahi-dKhan <shahidkhan12082004@gmail.com>
2026-04-06 19:51:03 +0530

    Started making a ViT

--- ViT.py
+++ ViT.py
@@ -17,4 +17,21 @@ print(device)
 model = timm.create_model('diet3_small_patch16_224', pretrained=True, num_classes = 10)
 model.to(device)
 
+img_dir = "A3_Dataset"
+train_csv = "A3_Dataset/train.csv"
+val_csv = "A3_Dataset/validation.csv"
+test_csv = "A3_Dataset/test.csv"
+EPOCHS = 10
+batch_size=32
+train_dataset = CropData(img_dir_path=img_dir, csv_file_path=train_csv)
+val_dataset = CropData(img_dir_path=img_dir, csv_file_path=val_csv)
+test_dataset = CropData(img_dir_path=img_dir, csv_file_path=test_csv)
+
+num_workers = 4
+pin_memory = device.type == "cuda"
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=pin_memory)
+val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=pin_memory)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=pin_memory)
+
+ 
 
